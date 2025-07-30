9b11b15d6efb2e51f9ae6ecfe85aec358fcc73ed
saurabh kumar <saurabh1sinha9324>
2025-07-30 16:23:06 +0530

    first commit

---
+++ components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <p>&copy; 2025 SMVITA. All rights reserved.</p>
+      <p>5th Floor, Vidyanidhi Complex, Juhu, Mumbai – 400 049</p>
+      <p>Email: training.vita@gmail.com | Phone: 022-26255629</p>
+    </footer>
+  );
+};
+
+export default Footer;
